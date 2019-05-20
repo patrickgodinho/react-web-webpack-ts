868bd35635806702657c0b8e9796366b39fccab1
Patrick <godinhopatrick@gmail.com>
2019-05-20 13:07:04 -0300

    simple hello world

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const rules = [
+  {
+    test: /\.tsx?/,
+    include: [path.resolve(__dirname, 'src')],
+    loader: 'babel-loader'
+  }
+];
+
+module.exports = {
+  target: 'web',
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: { rules },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devServer: {
+    contentBase: './',
+    port: 5000
+  }
+};
